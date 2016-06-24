44813c342c4086aa7a196c1b5ea87c028a13d220
Landon West <landonwest5@gmail.com>
2016-06-23 17:30:52 -0700

    create tweet and fb-share button jquery on-click functions

--- main.js
+++ main.js
@@ -7,9 +7,34 @@ function getQuote() {
   });
 }
 
+function tweet() {
+  $('#tweet-btn').on('click', function() {
+    var url = "https://twitter.com/intent/tweet?&text=\"" + quote + "\" R. Swanson&hashtags=ParksandRec&url=http://bit.ly/rswansqt";
+    $(this).attr('href', url);
+    window.open(url);
+  });
+}
+
+function fbShare() {
+  $('#fb-share-btn').on('click', function() {
+    FB.ui({
+        method: 'share',
+        display: 'popup',
+        href: 'http://codepen.io/Lando726/full/rLMxmd/',
+      }, function(response){
+        if (response && !response.error_message) {
+          alert('Posting completed.');
+        }
+      });
+  });
+}
+
 $(document).ready(function() {
   // populate html with api quote on button click
   $('#quote-btn').on('click', function() {
      getQuote();
   });
+
+  tweet();
+  fbShare();
 });
